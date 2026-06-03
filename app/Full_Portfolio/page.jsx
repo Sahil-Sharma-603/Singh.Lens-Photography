ca84f46661be2f0454452ed2ce8003668ff134ea
Sahil-Sharma-603 <sharmasahil8342@gmail.com>
2026-06-02 22:10:13 -0600

    full_portfolio section completed - next starting about section

--- app/Full_Portfolio/page.jsx
+++ app/Full_Portfolio/page.jsx
@@ -45,6 +45,7 @@ const portfolioImages = [
 
     },
 
+
 ]
 
 
@@ -60,19 +61,18 @@ const categories = [
 export default function Full_Portfolio() {
 
     const [activeCategory, setActiveCategory] = useState("All");
+    const [selectedIndex, setSelectedIndex] = useState(null);
 
-    const filterImages = [];
+    const filterImages =
+        activeCategory === "All"
+            ? portfolioImages
+            : portfolioImages.filter((image) => image.category === activeCategory);
 
-    if (activeCategory === "All") {
-        filterImages.push(...portfolioImages);
-    }else {
-        filterImages.push(...portfolioImages.filter(image => image.category === activeCategory));
-    }
 
     return (
         <section
             id="full_portfolio"
-            className="relative px-6 py-12 md:py-22"
+            className="relative px-6 py-12 md:py-24"
         >
             <div className="mx-auto max-w-5xl">
 
@@ -97,14 +97,14 @@ export default function Full_Portfolio() {
             </div>
 
 
-            <div className="flex px-6 py-6 justify-center gap-x-6">
+            <div className="flex px-6 py-6 justify-center gap-4 flex-wrap ">
                 {categories.map((category) => (
                     <button
                         key={category}
                         onClick={() => setActiveCategory(category)}
                         className={`rounded-full border px-6 py-3 text-sm font-semibold uppercase tracking-widest transition ${activeCategory === category
-                                ? "border-amber-400 bg-amber-400 text-black"
-                                : "border-zinc-600 text-zinc-200 hover:bg-amber-400 hover:text-black hover:border-amber-400"
+                            ? "border-amber-400 bg-amber-400 text-black"
+                            : "border-zinc-600 text-zinc-200 hover:bg-amber-400 hover:text-black hover:border-amber-400"
                             } `}
 
                     >
@@ -115,17 +115,74 @@ export default function Full_Portfolio() {
 
 
             <div className="px-6 mt-10 grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
-                {filterImages.map((image) => (
-                    <div key = {image.id} className='relative overflow-hidden rounded-lg'>
+                {filterImages.map((image, index) => (
+                    <div key={image.id} className=' group relative overflow-hidden rounded-lg'>
                         <img
-                            src = {image.src}
-                            alt = {image.title}
-                            className='h-80 w-full object-cover transition-transform duration-300 hover:scale-105'
+                            src={image.src}
+                            alt={image.title}
+                            onClick={() => setSelectedIndex(index)}
+                            className='h-80 w-full object-cover cursor-pointer transition-transform duration-500 hover:scale-110'
                         />
-                    
+
                     </div>
                 ))}
             </div>
+
+
+
+            {selectedIndex !== null && (
+                <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/90">
+                    <button
+                        onClick={() => setSelectedIndex(null)}
+                        className="absolute right-6 top-6 text-4xl text-white cursor-pointer"
+                    >   ×
+                    </button>
+
+
+                    {/* Image Wrapper */}
+                    <div className="relative">
+
+                        {/* Left Button */}
+                        <button
+                            className="absolute left-2 top-1/2 -translate-y-1/2 rounded-full bg-black/50 px-3 py-2 text-2xl text-white backdrop-blur transition hover:bg-amber-400 hover:text-black"
+                            onClick={() => {
+                                if (selectedIndex === 0) {
+                                    setSelectedIndex(filterImages.length - 1);
+                                } else {
+                                    setSelectedIndex(selectedIndex - 1);
+                                }
+                            }}
+                        >
+                            &lt;
+                        </button>
+
+                        {/* Enlarged Image */}
+                        <img
+                            src={filterImages[selectedIndex].src}
+                            alt={filterImages[selectedIndex].title}
+                            className="max-h-[90vh] max-w-[90vw] rounded-lg object-contain"
+                        />
+
+                        {/* Right Button */}
+                        <button
+                            className="absolute right-2 top-1/2 -translate-y-1/2 rounded-full bg-black/50 px-3 py-2 text-2xl text-white backdrop-blur transition hover:bg-amber-400 hover:text-black"
+                            onClick={() => {
+                                if (selectedIndex === filterImages.length - 1) {
+                                    setSelectedIndex(0);
+                                } else {
+                                    setSelectedIndex(selectedIndex + 1);
+                                }
+                            }}
+                        >
+                            &gt;
+                        </button>
+
+                    </div>
+
+                </div>
+            )}
+
+
 
         </section>
     )
